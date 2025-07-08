c1e20392c548f06f492049512357b464cfffa5c0
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2025-07-08 18:27:54 +0200

    Refactor deploy.py to generate unique image tags and streamline configuration loading

--- deploy.py
+++ deploy.py
@@ -2,10 +2,53 @@ import os
 import sys
 import subprocess
 import yaml
+import time
 
 from pathlib import Path
 
-def get_yaml_root_key_value(yaml_path, key):
+# Root dir = 3 levels up from current script location
+script_dir = Path(__file__).resolve().parent
+
+# Set the KUBECONFIG environment variable
+kubeconfig_path = script_dir / "files" / "local" / "k8s" / "users" / "k8s-lab-admin.kubeconfig"
+os.environ["KUBECONFIG"] = str(kubeconfig_path)
+
+def generate_tag():
+    """
+    Generates a unique tag based on the current Git commit hash and the current Unix timestamp.
+
+    Returns:
+      str: A tag string in the format "<git_commit_hash>-<timestamp>", where <git_commit_hash> is the
+         first 7 characters of the current Git commit hash and <timestamp> is the current time in seconds since the epoch.
+
+    Raises:
+      RuntimeError: If the Git commit hash cannot be retrieved.
+    """
+    try:
+      result = subprocess.run(["git", "rev-parse", "HEAD"], check=True, capture_output=True, text=True)
+      git_commit_hash = result.stdout.strip()[:7]
+
+      # Get current number of seconds since epoch
+      # Format the tag as "git_commit_hash-time_now"
+      tag = f"{git_commit_hash}-{int(time.time())}"
+      return tag
+    except subprocess.CalledProcessError as e:
+      raise RuntimeError(f"Failed to get Git commit hash: {e}")
+
+def get_config_from_yaml(yaml_path):
+    """
+    Loads and parses a YAML configuration file.
+
+    Args:
+      yaml_path (str): The path to the YAML file to be loaded.
+
+    Returns:
+      dict: The contents of the YAML file as a Python dictionary.
+
+    Raises:
+      FileNotFoundError: If the specified YAML file does not exist.
+      ValueError: If the YAML file cannot be parsed.
+    """
     if not os.path.exists(yaml_path):
         raise FileNotFoundError(f"File not found: {yaml_path}")
 
@@ -15,12 +58,13 @@ def get_yaml_root_key_value(yaml_path, key):
         except yaml.YAMLError as e:
             raise ValueError(f"Failed to parse YAML: {e}")
 
-    if key not in content:
-        raise KeyError(f"Key '{key}' not found at root level of {yaml_path}")
+    return content
 
-    return content[key]
+# Load the configuration from the YAML file
+config = get_config_from_yaml(script_dir / "config.yaml")
 
-def build_and_push_docker_image(project_root):
+# def build_and_push_docker_image(project_root, image_name, image_tag=None):
+def build_and_push_docker_image(app_name, app_dir, image_tag=None):
     """
     Builds and pushes a Docker image for the specified project.
 
@@ -44,28 +88,28 @@ def build_and_push_docker_image(project_root):
       - Ensure Docker is installed and running, and the user has permissions to
         build and push images.
     """
-    os.chdir(project_root)
+    global script_dir
+    global config
 
     # Read the registry host IP from YAML
-    config_path = project_root / "config.yaml"
-    docker_registry_host = get_yaml_root_key_value(config_path, "k8s_cplane_addr")
+    docker_registry_host = config.get("k8s_cplane_addr")
 
     # Path to the app
-    app_dir = project_root / "apps" / "todo" / "api"
     os.chdir(app_dir)
 
     # Build and push image
-    image = f"{docker_registry_host}:5000/k8s-lab/todod"
+    if image_tag is None:
+        image_tag = 'latest'
+    image = f"{docker_registry_host}:5000/k8s-lab/{app_name}:{image_tag}"
 
     print(f"📦 Building Docker image: {image}")
-    # subprocess.run(["docker", "build", ".", "--no-cache", "-t", image], check=True)
     subprocess.run(["docker", "build", ".", "-t", image], check=True)
 
     print(f"🚀 Pushing image to registry: {image}")
     subprocess.run(["docker", "push", image], check=True)
 
 
-def k8s_deploy_manifests(project_root):
+def k8s_deploy_manifests():
     """
     Deploys Kubernetes manifests for a project.
 
@@ -90,18 +134,11 @@ def k8s_deploy_manifests(project_root):
       KeyError: If the expected keys are not found in the YAML files.
     """
 
-    os.chdir(project_root)
-
-    # Set the KUBECONFIG environment variable
-    kubeconfig_path = project_root / "files" / "local" / "k8s" / "users" / "k8s-lab-admin.kubeconfig"
-    os.environ["KUBECONFIG"] = str(kubeconfig_path)
-
-    # Read the registry host IP from YAML
-    config_path = project_root / "config.yaml"
-    docker_registry_host = get_yaml_root_key_value(config_path, "k8s_cplane_addr")
+    global script_dir
+    os.chdir(script_dir)
 
     # Path to the manifests
-    manifests_dir = project_root / "k8s" / "manifests"
+    manifests_dir = script_dir / "k8s" / "manifests"
     # If there are no YAML files, return
     if not any(manifests_dir.glob("*.yaml")):
         print("No Kubernetes manifests found to apply.")
@@ -111,7 +148,7 @@ def k8s_deploy_manifests(project_root):
     subprocess.run(["kubectl", "apply", "-Rf", manifests_dir], check=True)
 
 
-def k8s_install_helm_charts(project_root):
+def k8s_install_helm_charts(image_tag):
     """
     Installs Helm charts for a project.
 
@@ -119,7 +156,7 @@ def k8s_install_helm_charts(project_root):
     and patches the deployment with the correct image.
 
     Args:
-      project_root (Path): The root directory of the project.
+      image_tag (str): The tag for the Docker image to be used in the deployment.
 
     Steps:
       1. Changes the working directory to the project root.
@@ -136,48 +173,49 @@ def k8s_install_helm_charts(project_root):
       KeyError: If the expected keys are not found in the YAML files.
     """
 
-    os.chdir(project_root)
-
-    # Set the KUBECONFIG environment variable
-    kubeconfig_path = project_root / "files" / "local" / "k8s" / "users" / "k8s-lab-admin.kubeconfig"
-    os.environ["KUBECONFIG"] = str(kubeconfig_path)
-
-    # Read the registry host IP from YAML
-    config_path = project_root / "config.yaml"
-    docker_registry_host = get_yaml_root_key_value(config_path, "k8s_cplane_addr")
+    global script_dir
+    global config
+    os.chdir(script_dir)
 
     # Path to Helm charts
-    helm_charts_dir = project_root / "k8s" / "charts"
+    helm_charts_dir = script_dir / "k8s" / "charts"
+
+    # docker registry host IP from YAML
+    docker_registry_host = config.get("k8s_cplane_addr")
 
     # Install Helm chart
-    print("Installing Helm chart for Todo application...")
     for directory in helm_charts_dir.iterdir():
         if directory.is_dir():
             release_name = directory.name
             chart = str(directory)
             print(f"📦 Installing Helm chart: {release_name}")
-            subprocess.run(["helm", "upgrade", "--install", release_name, chart,
-                            "-n", release_name, # Namespace for the release
-                            "--create-namespace", # Create namespace if it doesn't exist
-                            "--set", f"deployment.image.repository={docker_registry_host}:5000/k8s-lab/{release_name}"],
-                            check=True)
+            subprocess.run([
+                "helm", "upgrade", "--install", release_name, chart,
+                "-n", release_name,  # Namespace for the release
+                "--create-namespace",  # Create namespace if it doesn't exist
+                "--set", f"deployment.image.repository={docker_registry_host}:5000/k8s-lab/{release_name}",
+                "--set", f"deployment.image.tag={image_tag}"
+            ], check=True)
             print(f"🚀 Helm chart {release_name} installed successfully.")
 
 def main():
-    # Root dir = 3 levels up from current script location
-    script_dir = Path(__file__).resolve().parent
 
     # Build and push the Docker images
     print("🔨 Building and pushing Docker images...")
-    build_and_push_docker_image(script_dir)
+
+    # Get image tag
+    image_tag = generate_tag()
+
+    # Build todo application image
+    build_and_push_docker_image("todod", f"{script_dir}/apps/todo/api", image_tag=image_tag)
 
     # Apply base Kubernetes manifests
     print("📦 Applying Kubernetes manifests...")
-    k8s_deploy_manifests(script_dir)
+    k8s_deploy_manifests()
 
     # Install all Helm charts
     print("📦 Installing Helm charts...")
-    k8s_install_helm_charts(script_dir)
+    k8s_install_helm_charts(image_tag=image_tag)
 
 if __name__ == "__main__":
     try:
